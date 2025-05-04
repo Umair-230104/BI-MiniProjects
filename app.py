d5ea6271feffe8c39de40c760475739fd901d1bf
Masih Bijan Kabiri <144231041+masih1903@users.noreply.github.com>
2025-05-04 15:22:08 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from matplotlib.ticker import MultipleLocator
+import seaborn as sns
+from sklearn.decomposition import PCA
+
+
+st.title("DASHBOARD")
+st.write("Welcome to our wine data")
+
+
+    
